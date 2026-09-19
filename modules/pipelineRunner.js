1f8b46b1b00c25ab172b90076476fd0f58580d69
Sneha Suresh <techstack@knometrix.com>
2026-09-19 15:36:13 +0530

    remove unused highlightGenerator reference

--- modules/pipelineRunner.js
+++ modules/pipelineRunner.js
@@ -7,7 +7,6 @@ const { filterLowQualityArticles } = require('./qualityFilter');
 const { pushToProcessedQueue } = require('./processedQueue');
 const { startJobTracking, updateJobStage, markFullyCompleted, failJobTracking } = require('./jobStatusTracker');
 const { processQueueInBatches, FORWARD_OUTLOOK_MODULE_ID, MARKET_DYNAMICS_MODULE_ID } = require('./llmRelevanceProcessor');
-const { generateHighlight } = require('./highlightGenerator');
 
 // CHANGED: runPipeline now takes moduleId, threads it through dedup calls
 // and processQueueInBatches. Also tracks currentStage so a crash logs the
